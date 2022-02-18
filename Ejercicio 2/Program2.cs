05d3a2062faabe37dbdce71349deca1d43d48d0a
Jesus Alberto <Jesus Alberto@DESKTOP-O56G6NE>
2022-02-18 00:53:58 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio 2/Program2.cs
@@ -0,0 +1,8 @@
+﻿byte num1, num2;
+ushort multip;
+Console.WriteLine("Ingrese dos numeros de dos cifras ");
+num1= Convert.ToByte(Console.ReadLine());
+num2= Convert.ToByte(Console.ReadLine());
+multip = Convert.ToUInt16(num1 * num2);
+
+Console.WriteLine("\nla multiplicacion de los numeros es: " + multip);
---
+++ Ejercicio 3/Program3.cs
@@ -0,0 +1,12 @@
+﻿long num1, num2, suma, resta, producto;
+Console.WriteLine("Ingrese dos numeros enteros largos ");
+num1 = Convert.ToInt64(Console.ReadLine());
+num2 = Convert.ToInt64(Console.ReadLine());
+
+suma = num1 + num2;
+resta= num1 - num2;
+producto = num1 * num2;
+
+Console.WriteLine("\nLa suma es: " + suma);
+Console.WriteLine("\nLa resta es: " + resta);
+Console.WriteLine("\nEl producto es: " + producto);
